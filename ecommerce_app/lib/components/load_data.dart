64e627c9ff200acb23435ae2824194a508f25288
Akshay Gade <akshaygade2327@gmail.com>
2024-12-11 13:27:58 +0530

    ratings added

--- ecommerce_app/lib/components/load_data.dart
+++ ecommerce_app/lib/components/load_data.dart
@@ -52,6 +52,8 @@ class _LoadDataState extends State<LoadData> {
 
       allProducts = [];
       fetchedAllProducts.forEach((element) {
+        print(element['rating']['\$numberDecimal'].runtimeType);
+
         Product prod = Product(
             id: element['_id'],
             exporterId: element['exporterId'],
@@ -61,149 +63,150 @@ class _LoadDataState extends State<LoadData> {
             description: element['description'],
             availableQty: element['availableQty'],
             category: element['category'],
-            rating: element['rating'],
+            rating: double.parse(element['rating']['\$numberDecimal']),
             weight: element['weight'].toDouble(),
             isFestival: false,
             isPopular: true);
         allProducts.add(prod);
+        print("Done");
       });
-    }
 
-    print("all products listed");
-    if (allProducts.isEmpty) allProducts = demoProducts;
+      print("all products listed");
+      if (allProducts.isEmpty) allProducts = demoProducts;
 
-    print("Fetching festive products...");
+      print("Fetching festive products...");
 
-    List<dynamic> festiveResponse =
-        await FetchFestiveProducts.performHttpRequest(
-      'GET',
-      'utils/festive_products',
-      context,
-    );
-    if (festiveResponse.isNotEmpty) {
-      fetchedFestiveProducts = festiveResponse;
-      print("Festive products fetched successfully!");
-    } else {
-      print("No festive products found.");
-    }
-    festiveProducts = [];
+      List<dynamic> festiveResponse =
+          await FetchFestiveProducts.performHttpRequest(
+        'GET',
+        'utils/festive_products',
+        context,
+      );
+      if (festiveResponse.isNotEmpty) {
+        fetchedFestiveProducts = festiveResponse;
+        print("Festive products fetched successfully!");
+      } else {
+        print("No festive products found.");
+      }
+      festiveProducts = [];
 // Print fetched data to ensure it is populated correctly
-    // print(fetchedFestiveProducts);
+      // print(fetchedFestiveProducts);
 
-    if (fetchedFestiveProducts.isNotEmpty) {
-      fetchedFestiveProducts.forEach((element) {
-        Product prod = Product(
-          id: element['_id'],
-          exporterId: element['exporterId'],
-          title: element['productName'],
-          price: element['price'].toDouble(),
-          photoUrl: element['photoUrl'],
-          description: element['description'],
-          availableQty: element['availableQty'],
-          category: element['category'],
-          rating: element['rating'],
-          weight: element['weight'].toDouble(),
-          isFestival: true, // Explicitly setting it here
-          isPopular: false,
-        );
+      if (fetchedFestiveProducts.isNotEmpty) {
+        fetchedFestiveProducts.forEach((element) {
+          Product prod = Product(
+            id: element['_id'],
+            exporterId: element['exporterId'],
+            title: element['productName'],
+            price: element['price'].toDouble(),
+            photoUrl: element['photoUrl'],
+            description: element['description'],
+            availableQty: element['availableQty'],
+            category: element['category'],
+            rating: double.parse(element['rating']['\$numberDecimal']),
+            weight: element['weight'].toDouble(),
+            isFestival: true, // Explicitly setting it here
+            isPopular: false,
+          );
 
-        festiveProducts.add(prod);
-      });
-
-      // Verify if all products have isFestival set to true
-      // allProducts.forEach((prod) {
-      //   print('Product: ${prod.title}, isFestival: ${prod.isFestival}');
-      // });
-    }
-
-    //ORDER starting........................
-
-    List<dynamic> res = await FetchOrders.performHttpRequest(
-      'GET',
-      'consumer/getConsumerOrder/$currentUserEmail',
-    );
-
-    if (response.isNotEmpty) {
-      print("got orders");
-      allOrders = [];
-      orders = res;
-      // print(orders);
-
-      orders.forEach((element) async {
-        Map<String, dynamic> statusRes =
-            await FetchOrderStatus.performHttpRequest(
-          'GET',
-          "consumer/getOrderStatus/${element["_id"]}",
-        );
-        // print(statusRes);
-
-        //find highest prioriy and set dates
-        int statusIndex = 0;
-        Map<String, String> dateMap = {
-          "isDispatchedDate": "",
-          "isPickedUpDate": "",
-          "readyToShipDate": "",
-          "postmanAssignDate": "",
-          "dnkVerifiedDate": "",
-          "customVerifiedDate": "",
-          "isShipToCustomDate": "",
-          "isDroppedAtDnkDate": "",
-        };
-
-        statusRes.forEach((key, value) {
-          if (statusIdx.containsKey(key)) {
-            if (orderStatusMsgDateMap.containsKey(key) &&
-                (statusIdx[Key] ?? 0) > statusIndex)
-              statusIndex = statusIdx[key]!;
-            if (dateMap.containsKey(key)) dateMap[key] = value;
-          }
+          festiveProducts.add(prod);
         });
 
-        Order ord = Order(
-          dateMap: dateMap,
-          status: statusIndex,
-          id: element["_id"],
-          // date: element["orderDate"],
-          photoUrl: element["photoUrl"],
-          address: element["address"],
-          city: element["city"],
-          orderDate: element["orderDate"],
-          price: element["price"].toString(),
-          productName: element["productName"],
-          qty: element["qty"].toString(),
-          pincode: element["pincode"].toString(),
-          state: element["state"],
-        );
-        allOrders.add(ord);
-      });
-    }
+        // Verify if all products have isFestival set to true
+        // allProducts.forEach((prod) {
+        //   print('Product: ${prod.title}, isFestival: ${prod.isFestival}');
+        // });
+      }
 
-    //end of order.........................
+      //ORDER starting........................
 
-    if (currencySymbolMap.containsKey(countryName.toLowerCase()))
-      currencySymbol = currencySymbolMap[countryName.toLowerCase()]!;
+      List<dynamic> res = await FetchOrders.performHttpRequest(
+        'GET',
+        'consumer/getConsumerOrder/$currentUserEmail',
+      );
 
-    print(storedConversionRate);
+      if (response.isNotEmpty) {
+        print("got orders");
+        allOrders = [];
+        orders = res;
+        // print(orders);
 
-    if (storedConversionRate != 0) {
-      KConversionrate = storedConversionRate;
-      print("stored.....");
-      return KConversionrate;
-    } else {
-      var response = await http.get(Uri.parse(
-          "https://openexchangerates.org/api/latest.json?base=USD&app_id=" +
-              conversionKey));
-      var data = ratesModelFromJson(response.body);
-      var usd = 1 / data.rates["INR"]!;
+        orders.forEach((element) async {
+          Map<String, dynamic> statusRes =
+              await FetchOrderStatus.performHttpRequest(
+            'GET',
+            "consumer/getOrderStatus/${element["_id"]}",
+          );
+          // print(statusRes);
 
-      GcountryCode = CountryCodeMap[countryName.toLowerCase()]!;
-      KConversionrate = usd * data.rates[GcountryCode]!;
+          //find highest prioriy and set dates
+          int statusIndex = 0;
+          Map<String, String> dateMap = {
+            "isDispatchedDate": "",
+            "isPickedUpDate": "",
+            "readyToShipDate": "",
+            "postmanAssignDate": "",
+            "dnkVerifiedDate": "",
+            "customVerifiedDate": "",
+            "isShipToCustomDate": "",
+            "isDroppedAtDnkDate": "",
+          };
 
-      prefs.setDouble('conversionRate', KConversionrate);
-      print(currencySymbol);
-      print(KConversionrate);
+          statusRes.forEach((key, value) {
+            if (statusIdx.containsKey(key)) {
+              if (orderStatusMsgDateMap.containsKey(key) &&
+                  (statusIdx[Key] ?? 0) > statusIndex)
+                statusIndex = statusIdx[key]!;
+              if (dateMap.containsKey(key)) dateMap[key] = value;
+            }
+          });
 
-      return response;
+          Order ord = Order(
+            dateMap: dateMap,
+            status: statusIndex,
+            id: element["_id"],
+            // date: element["orderDate"],
+            photoUrl: element["photoUrl"],
+            address: element["address"],
+            city: element["city"],
+            orderDate: element["orderDate"],
+            price: element["price"].toString(),
+            productName: element["productName"],
+            qty: element["qty"].toString(),
+            pincode: element["pincode"].toString(),
+            state: element["state"],
+          );
+          allOrders.add(ord);
+        });
+      }
+
+      //end of order.........................
+
+      if (currencySymbolMap.containsKey(countryName.toLowerCase()))
+        currencySymbol = currencySymbolMap[countryName.toLowerCase()]!;
+
+      print(storedConversionRate);
+
+      if (storedConversionRate != 0) {
+        KConversionrate = storedConversionRate;
+        print("stored.....");
+        return KConversionrate;
+      } else {
+        var response = await http.get(Uri.parse(
+            "https://openexchangerates.org/api/latest.json?base=USD&app_id=" +
+                conversionKey));
+        var data = ratesModelFromJson(response.body);
+        var usd = 1 / data.rates["INR"]!;
+
+        GcountryCode = CountryCodeMap[countryName.toLowerCase()]!;
+        KConversionrate = usd * data.rates[GcountryCode]!;
+
+        prefs.setDouble('conversionRate', KConversionrate);
+        print(currencySymbol);
+        print(KConversionrate);
+
+        return response;
+      }
     }
   }
 
